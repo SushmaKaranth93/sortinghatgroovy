ec0198974f1bce74eb8c07984cabf6ee593e9d92
sushma <sushma@channelbridge.in>
2021-08-31 22:43:50 +0530

    feat: store student data

--- src/com/vendantu/sortinghat/SortingHat.groovy
+++ src/com/vendantu/sortinghat/SortingHat.groovy
@@ -8,14 +8,23 @@ class SortingHat {
         println(" Enter the number of elements you want to store: \n");
         numberOfElements=sc.nextInt();
         sc.skip(System.lineSeparator());
-
+        List<String> studentRegisterDataInput=[];
         println(" Enter the 4 digit register number, class (A/B) and food preferences (V/NV)  like given below \n eg: 1001 B NV");
         for(int i=0;i<numberOfElements;i++){
             String studentInput=sc.nextLine();
-            studentInput << studentInput;
+            studentRegisterDataInput << studentInput;
         }
         sc.close()
 
-        List<String> studentRegisterData=[];
+        List<Map> studentRegisterData = [];
+        studentRegisterData= (0..(numberOfElements-1)).collect{it->
+            List<String> studentList = studentRegisterDataInput [it].split(" ").toList();
+            Map student= [:]
+            student.registerNumber = studentList[0]?.toInteger();
+            student.batch = studentList[1]?.toInteger();
+            student.preference = studentList[2]?.toInteger();
+            student
+        }
+
     }
 }
